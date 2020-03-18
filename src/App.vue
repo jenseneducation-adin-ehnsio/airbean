5e37f710e4b43fdd0be7c657947efbc0b1b063fe
jenseneducation-adin-ehnsio <55272975+jenseneducation-adin-ehnsio@users.noreply.github.com>
2020-03-18 17:01:55 +0100

    basic css settings for root

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/order">Order</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -18,24 +13,21 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+body {
+  margin: 0;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  max-width: 375px;
+  min-height: 812px;
+  border: 1px solid black;
+  margin: 0 auto;
 }
 </style>
