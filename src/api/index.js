f95c6d18912b9301ae26de1337d03ade19edd560
jenseneducation-sharon-tan <54704821+jenseneducation-sharon-tan@users.noreply.github.com>
2020-03-19 13:56:14 +0100

    Loading screen completed with css

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
-})
+  baseURL: "http://localhost:5000/api"
+});
 
-async function fetchMenu(){
-    const response = await api.get('/beans')
-    const data = response.data.menu
-    return data
+async function fetchMenu() {
+  const response = await api.get("/beans");
+  const data = response.data.menu;
+  return data;
 }
 
-export {fetchMenu}
+export { fetchMenu };
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="menu">
-    <p>{{menu}}</p>
+    <LoadingScreen />
+    <!--  <p>{{menu}}</p> -->
   </div>
 </template>
 
 <script>
-
-
+import LoadingScreen from "@/components/LoadingScreen.vue";
 export default {
+  components: { LoadingScreen },
   computed: {
     menu() {
-      return this.$store.state.menu
+      return this.$store.state.menu;
     }
-  },
-}
+  }
+};
 </script>
