3c5fafae8bc454118a74d58b1687e184ef928ada
jenseneducation-sharon-tan <54704821+jenseneducation-sharon-tan@users.noreply.github.com>
2020-03-19 23:50:19 +0100

    loading done but not nav

--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="menu">
-    <LoadingScreen />
-    <!--  <p>{{menu}}</p> -->
+    <!--  <LoadingScreen /> -->
+    <NavButton />
+    <Nav />
+    <!--  <p>{{ menu }}</p> -->
   </div>
 </template>
 
 <script>
-import LoadingScreen from "@/components/LoadingScreen.vue";
+/* import LoadingScreen from "@/components/LoadingScreen"; */
+import NavButton from "@/components/NavButton";
+import Nav from "@/components/Nav";
 export default {
-  components: { LoadingScreen },
+  components: { /* LoadingScreen, */ NavButton, Nav },
+
   computed: {
     menu() {
       return this.$store.state.menu;
