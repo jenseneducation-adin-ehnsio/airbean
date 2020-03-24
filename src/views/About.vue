f6bace801e3ec1bc950fbf7525ea2819e21e1a85
jenseneducation-sharon-tan <54704821+jenseneducation-sharon-tan@users.noreply.github.com>
2020-03-24 16:37:39 +0100

    loading screen with transition and overlay done

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="about">
+    <Nav v-bind:showNav="navVisible" v-on:navClose="closeNav" />
     <header>
       <img src="../assets/graphics-header.svg" class="border-top" />
-      <NavButton class="navButton" v-bind:showNav="navVisible" v-on:navOpen="openNav" />
-      <Nav v-bind:showNav="navVisible" v-on:navClose="closeNav" />
+      <NavButton
+        class="navButton"
+        v-bind:showNav="navVisible"
+        v-on:navOpen="openNav"
+      />
     </header>
     <section class="info">
       <h1>Vårt kaffe</h1>
       <p>
-        <strong>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus dolorum unde obcaecati sint, officiis molestias incidunt voluptates inventore, ut omnis odit. Animi architecto rerum porro nihil
-          , numquam reprehenderit placeat libero.
-        </strong>
+        <strong
+          >Pumpkin spice mug, barista cup, sit macchiato, kopi-luwak, doppio,
+          grounds dripper, crema, strong whipped, variety extra iced id lungo
+          half and half mazagran. Pumpkin spice.</strong
+        >
       </p>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum impedit nesciunt nam perspiciatis veritatis natus sequi suscipit aperiam autem sit hic ipsa,
-        ipsam ea optio atque accusamus facere voluptatum quidem.
+        Que dark fair trade, spoon decaffeinated, barista wings whipped, as rich
+        aftertaste, con panna milk black, arabica white rich beans single shot
+        extra affogato. So affogato macchiato sit extraction instant grinder
+        seasonal organic, turkish single shot, single origin, and robusta strong
+        to go so dripper. Viennese froth, grounds caramelization skinny aromatic
+        cup kopi-luwak, fair trade flavour, frappuccino medium, café au lait
+        flavour cultivar ut bar instant kopi-luwak.
       </p>
+
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto animi ut error sequi, cumque molestiae at est officia non optio vel deleniti
-        quis harum ratione quisquam porro commodi suscipit explicabo!
+        Roast id macchiato, single shot siphon mazagran milk fair trade est
+        aroma a half and half and, so, galão iced to go, whipped as cream cup
+        pumpkin spice iced. At extra, rich grinder, brewed to go, steamed half
+        and half at, that, percolator macchiato trifecta and body as arabica
+        dripper. In galão black java milk sit trifecta, robusta, acerbic café au
+        lait instant shop latte. Seasonal bar shop filter aroma id, crema,
+        affogato viennese cultivar aftertaste, seasonal, percolator cream black,
+        galão flavour, milk aromatic turkish skinny crema.
       </p>
     </section>
     <section class="profile">
@@ -58,6 +76,7 @@ export default {
 <style lang="scss" scoped>
 .about {
   background: #fdeded;
+  position: relative;
 }
 
 .border-top,
@@ -66,8 +85,8 @@ export default {
 }
 .navButton {
   position: absolute;
-  margin-top: 10px;
-  margin-left: -480px;
+  margin-top: 25px;
+  margin-left: -475px;
 }
 h1 {
   font-size: 3rem;
@@ -75,8 +94,12 @@ h1 {
 .info {
   text-align: left;
   padding: 0 50px;
-  line-height: 25px;
+  line-height: 30px;
   margin-bottom: 80px;
+}
+
+p {
+  margin-bottom: 30px;
 }
 
 .profile-eva {
