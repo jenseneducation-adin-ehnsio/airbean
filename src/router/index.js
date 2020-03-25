b2b9296b05e5f9f835dc37e58911b6125cb9eedd
jenseneducation-sharon-tan <54704821+jenseneducation-sharon-tan@users.noreply.github.com>
2020-03-25 15:34:11 +0100

    added profile page

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,39 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Menu from '@/views/Menu.vue'
-import About from '@/views/About.vue'
-import Order from '@/views/Order.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Menu from "@/views/Menu.vue";
+import About from "@/views/About.vue";
+import Order from "@/views/Order.vue";
+import Profile from "@/views/OrderProfile.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Menu',
+    path: "/",
+    name: "Menu",
     component: Menu
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     component: About
   },
   {
-    path: '/order',
-    name: 'Order',
+    path: "/order",
+    name: "Order",
     component: Order
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
