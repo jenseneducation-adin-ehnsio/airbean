17499fb850024e14cb433f8dc2da4b7a8d13de17
jenseneducation-sharon-tan <54704821+jenseneducation-sharon-tan@users.noreply.github.com>
2020-03-30 16:11:54 +0200

    no changes

--- backend/routes/beans.js
+++ backend/routes/beans.js
@@ -1,44 +1,43 @@
-const { Router } = require('express')
-const router = new Router()
-const fs = require('fs');
-const uuid = require('uuid-random');
-const { generateOrderNr, generateETA } = require('../utils/utils');
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync('db.json')
-const db = low(adapter)
+const { Router } = require("express");
+const router = new Router();
+const fs = require("fs");
+const uuid = require("uuid-random");
+const { generateOrderNr, generateETA } = require("../utils/utils");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
 
-
-router.get('/', async (req, res) => {
-    const menu = fs.createReadStream('data/menu.json');
-    menu.pipe(res);
+router.get("/", async (req, res) => {
+  const menu = fs.createReadStream("data/menu.json");
+  menu.pipe(res);
 });
 
-router.post('/', async (req, res) => {
-    const order = {
-        eta: generateETA(),
-        orderNr: generateOrderNr(),
-    }
+router.post("/", async (req, res) => {
+  const order = {
+    eta: generateETA(),
+    orderNr: generateOrderNr()
+  };
 
-    db.get('orders') // Ska hämta orders efter uuid (key, se funktion nedan)
-    .push({ 
-        orderNumber: order.orderNr, 
-        timeStamp: Date.now(), 
-        Items: req.body.items, 
-        totalValue: req.body.value
+  db.get("orders") // Ska hämta orders efter uuid (key, se funktion nedan)
+    .push({
+      orderNumber: order.orderNr,
+      timeStamp: Date.now(),
+      Items: req.body.items,
+      totalValue: req.body.value
     })
-    .write()
-    
-    setTimeout(() => {
-        res.send(order);
-    }, 2000);
+    .write();
+
+  setTimeout(() => {
+    res.send(order);
+  }, 2000);
 });
 
-router.get('/key', (req, res) => {
-    const key = {
-        key: uuid()
-    }
-    res.send(JSON.stringify(key));
-})
+router.get("/key", (req, res) => {
+  const key = {
+    key: uuid()
+  };
+  res.send(JSON.stringify(key));
+});
 
-module.exports = router
+module.exports = router;
